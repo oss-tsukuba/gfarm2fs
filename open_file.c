a80e94bda37257fea5caf55058063a09dee96279
Osamu Tatebe <tatebe@users.sourceforge.net>
2010-06-25 06:35:27 +0000

    fix segfault when a metadata server fails over
    git-svn-id: https://svn.code.sf.net/p/gfarm/code/gfarm2fs/trunk@4781 4a5e1ece-6a47-475c-b646-731d720b41d0

--- open_file.c
+++ open_file.c
@@ -19,7 +19,20 @@ struct gfarm_hash_entry *gfarm_hash_enter(struct gfarm_hash_table *,
 	const void *, int, int, int *);
 int gfarm_hash_purge(struct gfarm_hash_table *,
 	const void *, int);
+void *gfarm_hash_entry_key(struct gfarm_hash_entry *);
 void *gfarm_hash_entry_data(struct gfarm_hash_entry *);
+
+struct gfarm_hash_iterator {
+	struct gfarm_hash_table *table;
+	int bucket_index;
+	struct gfarm_hash_entry **pp;
+};
+void gfarm_hash_iterator_begin(struct gfarm_hash_table *,
+	struct gfarm_hash_iterator *);
+void gfarm_hash_iterator_next(struct gfarm_hash_iterator *);
+int gfarm_hash_iterator_is_end(struct gfarm_hash_iterator *);
+struct gfarm_hash_entry *gfarm_hash_iterator_access(
+	struct gfarm_hash_iterator *);
 
 #include "gfarm2fs.h"
 
@@ -111,7 +124,7 @@ gfarm2fs_open_file_enter(GFS_File gf, int flags)
 
 	if (get_ino(gf, &ino) != 0) {
 		gflog_debug(GFARM_MSG_UNFIXED,
-		    "file %p does not exist in the open file table", gf);
+		    "file %p does not exist", gf);
 		return;
 	}
 	o = malloc(sizeof(*o));
@@ -125,7 +138,7 @@ gfarm2fs_open_file_enter(GFS_File gf, int flags)
 	    sizeof(*ios), &created);
 	if (entry == NULL) {
 		gflog_error(GFARM_MSG_UNFIXED,
-		    "no memory to insert inode %lld to the open file table",
+		    "no memory to insert inode %lld to open file table",
 		    (unsigned long long)ino);
 		return;
 	}
@@ -145,38 +158,75 @@ gfarm2fs_open_file_enter(GFS_File gf, int flags)
 		ios->gf_cached = gf;
 }
 
+static int
+open_file_remove_opening(struct inode_openings *ios, GFS_File gf)
+{
+	struct opening *o, **prev;
+
+	for (prev = &ios->openings; (o = *prev) != NULL; prev = &o->next) {
+		if (o->gf == gf)
+			break;
+	}
+	if (o == NULL)
+		return (1);
+
+	*prev = o->next;
+	free(o);
+	return (0);
+}
+
+static int
+get_ino_and_remove_opening_from_open_table(GFS_File gf,
+	gfarm_ino_t *inop, struct inode_openings **iosp)
+{
+	struct gfarm_hash_iterator it;
+	struct gfarm_hash_entry *entry = NULL;
+	struct inode_openings *ios = NULL;
+
+	for (gfarm_hash_iterator_begin(open_file_table, &it);
+	    !gfarm_hash_iterator_is_end(&it); gfarm_hash_iterator_next(&it)) {
+		entry = gfarm_hash_iterator_access(&it);
+		ios = gfarm_hash_entry_data(entry);
+
+		if (open_file_remove_opening(ios, gf) == 0)
+			break;
+	}
+	if (gfarm_hash_iterator_is_end(&it))
+		return (1);
+
+	*inop = *(gfarm_ino_t *)gfarm_hash_entry_key(entry);
+	*iosp = ios;
+	return (0);
+}
+
 void
 gfarm2fs_open_file_remove(GFS_File gf)
 {
 	gfarm_ino_t ino;
 	struct gfarm_hash_entry *entry;
-	struct inode_openings *ios;
-	struct opening *o, **prev;
+	struct inode_openings *ios = NULL;
 
 	if (get_ino(gf, &ino) != 0) {
-		gflog_debug(GFARM_MSG_UNFIXED,
-		    "file %p does not exist in the open file table", gf);
-		return;
-	}
-	entry = gfarm_hash_lookup(open_file_table, &ino, sizeof(ino));
-	if (entry == NULL) {
-		gflog_info(GFARM_MSG_UNFIXED,
-		    "inode %lld is not found in the open file table",
-		    (unsigned long long)ino);
-		return;
-	}
-	ios = gfarm_hash_entry_data(entry);
-	for (prev = &ios->openings; (o = *prev) != NULL; prev = &o->next) {
-		if (o->gf == gf)
-			break;
-	}
-	if (o == NULL) {
-		gflog_info(GFARM_MSG_UNFIXED,
-		    "a GFS_File is not found in the inode %lld openings",
-		    (unsigned long long)ino);
+		if (get_ino_and_remove_opening_from_open_table(gf, &ino, &ios)
+		    != 0) {
+			gflog_warning(GFARM_MSG_UNFIXED,
+			    "file %p does not exist in open file table", gf);
+			return;
+		}
+		/* the opening entry removed */
 	} else {
-		*prev = o->next;
-		free(o);
+		entry = gfarm_hash_lookup(open_file_table, &ino, sizeof(ino));
+		if (entry == NULL) {
+			gflog_warning(GFARM_MSG_UNFIXED,
+			    "inode %lld is not found in open file table",
+			    (unsigned long long)ino);
+			return;
+		}
+		ios = gfarm_hash_entry_data(entry);
+		if (open_file_remove_opening(ios, gf) != 0)
+			gflog_warning(GFARM_MSG_UNFIXED,
+			    "file %p is not found in the inode %lld openings",
+			    gf, (unsigned long long)ino);
 	}
 	if (ios->gf_cached == gf)
 		ios->gf_cached = NULL;
